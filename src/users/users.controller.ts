fdcce1daced896f68bbbef362157bc2b718c7e51
codezera <codezera3@gmail.com>
2023-12-30 14:49:20 +0530

    added CRUD routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // Get all users /users
+  //   @Get()
+  //   findAll() {
+  //     return [];
+  //   }
+
+  // Get with query params
+  @Get()
+  getUsers(@Query('role') role: 'ADMIN' | 'MANAGER') {
+    return { role };
+  }
+
+  // Get One User /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  //   Post /users
+  @Post()
+  createUser(@Body() user: any) {
+    return user;
+  }
+
+  //   Patch /users/:id
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() user: any) {
+    return { id, ...user };
+  }
+
+  //   Delete /users/:id
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return { response: `User with id ${id} deleted successfully` };
+  }
+}
